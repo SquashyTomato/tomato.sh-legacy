f42f4a6c1fc77f09fba2dff12461986467c8f6e1
SquashyTomato <lgcheesecakeyt@gmail.com>
2020-06-11 04:00:06 +0100

    Added Email Go Link

--- data/go.php
+++ data/go.php
@@ -4,6 +4,7 @@ $go = [
     'twitch' => 'https://twitch.com/SquashyTomato',
     'discord' => 'https://discord.gg/xxFFqq2',
     'osu' => 'https://osu.ppy.sh/users/9239206',
-    'git' => 'https://gitlab.com/SquashyTomato'
+    'git' => 'https://gitlab.com/SquashyTomato',
+    'email' => 'mailto:me@tomato.sh?subject=Hello There!'
 ]
 ?>
